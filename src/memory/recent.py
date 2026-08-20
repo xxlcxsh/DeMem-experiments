3f6d9e722e897f76af22ea051f170e0fbadc58ff
xxlcxsh <xxlcxsh@gmail.com>
2026-08-20 23:26:02 +0300

    recent memory reworked

--- src/memory/recent.py
+++ src/memory/recent.py
@@ -3,5 +3,6 @@ class RecentMemory(Memory):
     def retrieve(self, situation, topk = 5):
         return self.memory[-topk:]
     def update(self, situation, predicted_action, step_result):
-        self.memory.append(MemoryEntry(text=situation.text,action=situation.correct_action))
-        if len(self.memory) == (self.capacity+1): self.memory.pop(0)
+        self.memory.append(MemoryEntry(text=situation.text, action=step_result.correct_action))
+        if len(self.memory) > self.capacity:
+            self.memory.pop(0)
